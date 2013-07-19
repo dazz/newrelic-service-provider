46bfa3cb1fcae1441c4d6b2b51e77190cda3cdca
dazz <annejulia@gmail.com>
2013-07-19 21:13:34 +0200

    added all newrelic functions to ReporterInterface

--- src/Silex/Provider/Newrelic/Reporter.php
+++ src/Silex/Provider/Newrelic/Reporter.php
@@ -29,14 +29,14 @@ class Reporter implements ReporterInterface
     }
 
     /**
-     * @param string $appname
+     * @param string $appName
      * @param string $license
      *
      * @link https://newrelic.com/docs/php/the-php-api#api-start-txn
      */
-    public function startTransaction($appname, $license = '')
+    public function startTransaction($appName, $license = '')
     {
-        newrelic_start_transaction($appname, $license);
+        newrelic_start_transaction($appName, $license);
     }
 
     /**
@@ -50,7 +50,7 @@ class Reporter implements ReporterInterface
     /**
      * @link https://newrelic.com/docs/php/the-php-api#api-ignore-transaction
      */
-    public function ignoreTransacion()
+    public function ignoreTransaction()
     {
         newrelic_ignore_transaction();
     }
@@ -79,15 +79,62 @@ class Reporter implements ReporterInterface
         newrelic_capture_params ($flag);
     }
 
-    /*
-newrelic_custom_metric (metric_name, value)show details
-newrelic_add_custom_parameter (key, value)show details
-newrelic_add_custom_tracer (function_name)
-newrelic_add_custom_tracer (classname::function_name)show details
-newrelic_get_browser_timing_header ( [flag] )show details
-newrelic_get_browser_timing_footer ([ flag] )show details
-newrelic_disable_autorum (  )show details
-newrelic_set_user_attributes (user, account, product)show details
+    /**
+     * @param string $metricName
+     * @param int    $value  Values saved are assumed to be milliseconds, so "4" will be stored as ".004"
+     * @param string $prefix Avoid creating too many unique custom metric names.
+     *
+     * @link https://newrelic.com/docs/php/the-php-api#api-custom-metric
+     */
+    public function addCustomMetric($metricName, $value, $prefix = 'Custom')
+    {
+        newrelic_custom_metric(sprint('%s/%s', $prefix, $metricName), $value);
+    }
 
-    */
+    /**
+     * This parameter is shown in any transaction trace that results from this transaction.
+     *
+     * @param string $key
+     * @param        $value
+     *
+     * @link https://newrelic.com/docs/php/the-php-api#api-custom-param
+     */
+    public function addCustomParameter($key, $value)
+    {
+        newrelic_add_custom_parameter($key, $value);
+    }
+
+    /**
+     *
+     * @param string $functionOrClassName
+     *
+     * @example: $functionOrClassName = function_name or classname::function_name
+     *
+     * @link   https://newrelic.com/docs/php/the-php-api#api-custom-tracer
+     */
+    public function addCustomTracer($functionOrClassName)
+    {
+        newrelic_add_custom_tracer($functionOrClassName);
+    }
+
+    /**
+     * @param bool $flag If flag is specified it must be a boolean, and if omitted, defaults to true
+     *
+     * @link https://newrelic.com/docs/php/the-php-api#api-rum-header
+     */
+    public function getBrowserTimingHeader($flag = true)
+    {
+        newrelic_get_browser_timing_header($flag);
+    }
+
+
+    public function disableAutorun()
+    {
+        newrelic_disable_autorum();
+    }
+
+    public function setUserAttributes($user, $account, $product)
+    {
+        newrelic_set_user_attributes ($user, $account, $product);
+    }
 }
--- src/Silex/Provider/Newrelic/ReporterInterface.php
+++ src/Silex/Provider/Newrelic/ReporterInterface.php
@@ -14,5 +14,120 @@ namespace Dazz\Silex\Provider\Newrelic;
  */
 interface ReporterInterface
 {
+    /**
+     * @param string     $message
+     * @param \Exception $exception
+     *
+     * @link https://newrelic.com/docs/php/the-php-api#api-notice-error
+     */
+    public function addNoticeError($message, \Exception $exception = null);
 
+    /**
+     * @param string $name
+     *
+     * @link https://newrelic.com/docs/php/the-php-api#api-name-wt
+     */
+    public function setTransactionName($name);
+
+    /**
+     * @param string $appName
+     * @param string $license
+     *
+     * @link https://newrelic.com/docs/php/the-php-api#api-start-txn
+     */
+    public function startTransaction($appName, $license = '');
+
+    /**
+     * @link https://newrelic.com/docs/php/the-php-api#api-end-txn
+     */
+    public function endTransaction();
+
+    /**
+     * @link https://newrelic.com/docs/php/the-php-api#api-ignore-transaction
+     */
+    public function ignoreTransaction();
+
+    /**
+     * Do not generate Apdex metrics for this transaction.
+     *
+     * Useful when you have either very short or very long transactions
+     *   (such as file downloads) that can skew your apdex score
+     *
+     * @link https://newrelic.com/docs/php/the-php-api#api-ignore-apdex
+     */
+    public function ignoreApdex();
+
+    /**
+     * @param bool $flag
+     *
+     * @link https://newrelic.com/docs/php/the-php-api#api-bg
+     */
+    public function isBackgroundJob($flag = false);
+
+    /**
+     * @param bool $flag
+     *
+     * @link https://newrelic.com/docs/php/the-php-api#api-capture-params
+     */
+    public function captureParams($flag = true);
+
+    /**
+     * @param string $metricName
+     * @param int    $value  Values saved are assumed to be milliseconds, so "4" will be stored as ".004"
+     * @param string $prefix Avoid creating too many unique custom metric names.
+     *
+     * @link https://newrelic.com/docs/php/the-php-api#api-custom-metric
+     */
+    public function addCustomMetric($metricName, $value, $prefix);
+
+    /**
+     * This parameter is shown in any transaction trace that results from this transaction.
+     *
+     * @param string $key
+     * @param        $value
+     *
+     * @link https://newrelic.com/docs/php/the-php-api#api-custom-param
+     */
+    public function addCustomParameter($key, $value);
+
+    /**
+     * @param string $functionOrClassName
+     *
+     * @example: $functionOrClassName = function_name or classname::function_name
+     *
+     * @link https://newrelic.com/docs/php/the-php-api#api-custom-tracer
+     */
+    public function addCustomTracer($functionOrClassName);
+
+    /**
+     * @param bool $flag If flag is specified it must be a boolean, and if omitted, defaults to true
+     *
+     * @link https://newrelic.com/docs/php/the-php-api#api-rum-header
+     */
+    public function getBrowserTimingHeader($flag = true);
+
+    /**
+     * @param bool $flag If flag is specified it must be a boolean, and if omitted, defaults to true.
+     *
+     * @link https://newrelic.com/docs/php/the-php-api#api-rum-footer
+     */
+    public function getBrowserTimingFooter($flag = true);
+
+    /**
+     * Prevents the output filter from attempting to insert
+     *  RUM JavaScript for this current transaction. Useful for AJAX calls, for example
+     *
+     * @link https://newrelic.com/docs/php/the-php-api#api-rum-disable
+     */
+    public function disableAutoRum();
+
+    /**
+     * @param string $user
+     * @param string $account
+     * @param string $product
+     *
+     * @link https://newrelic.com/docs/php/the-php-api#api-set-user-attributes
+     * @link https://newrelic.com/docs/features/browser-traces
+     */
+    public function setUserAttributes($user, $account, $product);
 }
